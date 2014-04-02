e8111f6e5f4c2f1d4ed1b6c33e61252c685232d9
Bruno Gomes <bgosiris@gmail.com>
2014-04-02 17:46:58 +0100

    Add fields to payin entity to deal with 3D secure

--- mangopaysdk/entities/payin.py
+++ mangopaysdk/entities/payin.py
@@ -3,7 +3,7 @@ from mangopaysdk.entities.transaction import Transaction
 
 class PayIn (Transaction):
 
-    def __init__(self, id = None):
+    def __init__(self, id=None):
         self.CreditedWalletId = None
         # PayInPaymentType {CARD, BANK_WIRE, AUTOMATIC_DEBIT, DIRECT_DEBIT, PREAUTHORIZED }
         self.PaymentType = None
@@ -13,24 +13,27 @@ class PayIn (Transaction):
         self.ExecutionType = None
         # One of PayInExecutionDetails implementations, depending on ExecutionType
         self.ExecutionDetails = None
-        return super(PayIn, self).__init__(id)    
-    
+        # Required for 3D Secure
+        self.SecureModeNeeded = None
+        self.SecureModeRedirectURL = None
+        return super(PayIn, self).__init__(id)
+
     def GetDependsObjects(self):
-        return { 
-                'PaymentType': {
-                    '_property_name': 'PaymentDetails', 
-                    'CARD': 'PayInPaymentDetailsCard',
-                    'PREAUTHORIZED': 'PayInPaymentDetailsPreAuthorized',
-                    'BANK_WIRE': 'PayInPaymentDetailsBankWire'
-                }, 'ExecutionType': {
-                    '_property_name': 'ExecutionDetails', 
-                    'WEB': 'PayInExecutionDetailsWeb',
-                    'DIRECT': 'PayInExecutionDetailsDirect'                
-                }
+        return {
+            'PaymentType': {
+                '_property_name': 'PaymentDetails',
+                'CARD': 'PayInPaymentDetailsCard',
+                'PREAUTHORIZED': 'PayInPaymentDetailsPreAuthorized',
+                'BANK_WIRE': 'PayInPaymentDetailsBankWire'
+            }, 'ExecutionType': {
+                '_property_name': 'ExecutionDetails',
+                'WEB': 'PayInExecutionDetailsWeb',
+                'DIRECT': 'PayInExecutionDetailsDirect'
+            }
         }
 
     def GetReadOnlyProperties(self):
         properties = super(PayIn, self).GetReadOnlyProperties()
-        properties.append('PaymentType' )        
-        properties.append('ExecutionType' )        
+        properties.append('PaymentType')
+        properties.append('ExecutionType')
         return properties
