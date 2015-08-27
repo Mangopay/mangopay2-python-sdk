16e966358f95c2db1158538210b32a0ece0b69bb
Hugo Bailey <hobailey@users.noreply.github.com>
2015-08-27 19:02:42 +0200

    Update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 # to build - python setup.py sdist upload
 setup(
     name='mangopaysdk',
-    version='1.5.2',
+    version='1.8',
     author='Mangopay (www.mangopay.com)',
     author_email='support@mangopay.com',
     packages=['mangopaysdk', 'mangopaysdk.entities', 'mangopaysdk.tools', 'mangopaysdk.tools.storages', 'mangopaysdk.types', 'mangopaysdk.types.exceptions'],
