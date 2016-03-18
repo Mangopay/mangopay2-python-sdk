dbacceddb5605c6bc43aec6633ae7cf4f4f24c48
lukasz-drzewiecki <lukasz.drzewiecki@mosaic-lab.com>
2016-03-18 10:16:03 +0100

    Implemented payins by PayPal.

---
+++ mangopaysdk/types/payinpaymentdetailspaypal.py
@@ -0,0 +1,4 @@
+﻿from mangopaysdk.types.payinpaymentdetails import PayInPaymentDetails
+
+class PayInPaymentDetailsPayPal(PayInPaymentDetails):
+    """Class represents PayPal type for mean of payment in PayIn entity."""
