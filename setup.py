f173508e201a34b9821d9561beef192000f19f7e
Hugo Bailey <hobailey@users.noreply.github.com>
2016-10-26 19:00:17 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 # to build - python setup.py sdist upload
 setup(
     name='mangopaysdk',
-    version='2.1.0',
+    version='2.1.1',
     author='Mangopay (www.mangopay.com)',
     author_email='support@mangopay.com',
     packages=['mangopaysdk', 'mangopaysdk.entities', 'mangopaysdk.tools', 'mangopaysdk.tools.storages', 'mangopaysdk.types', 'mangopaysdk.types.exceptions'],
