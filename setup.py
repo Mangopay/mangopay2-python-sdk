9060396d34a8b20dcefcd3452033b40533d9da7e
Hugo Bailey <hobailey@users.noreply.github.com>
2016-11-21 18:56:37 +0100

    Change description reference

--- setup.py
+++ setup.py
@@ -2,9 +2,7 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
-here = path.abspath(path.dirname(__file__))
-
-with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
+with open('DESCRIPTION.rst') as f:
     long_description = f.read()
 
 setup(
