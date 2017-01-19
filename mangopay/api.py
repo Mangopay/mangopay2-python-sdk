b561f768e1143b79c830dc9dff0be8a7678f1afc
stefanGhivi <ghivi.stef@gmail.com>
2017-01-19 18:30:40 +0200

    add request timeout

--- mangopay/api.py
+++ mangopay/api.py
@@ -14,7 +14,7 @@ from .exceptions import APIError, DecodeError
 from .signals import request_finished, request_started, request_error
 from .utils import reraise_as, truncatechars
 
-from requests.exceptions import ConnectionError
+from requests.exceptions import ConnectionError, ConnectTimeout, Timeout
 
 try:
     import urllib.parse as urlrequest
@@ -33,7 +33,8 @@ requests_session = requests.Session()
 
 
 class APIRequest(object):
-    def __init__(self, client_id=None, passphrase=None, api_url=None, api_sandbox_url=None, sandbox=True, storage_strategy=None):
+    def __init__(self, client_id=None, passphrase=None, api_url=None, api_sandbox_url=None, sandbox=True,
+                 timeout=30.0, storage_strategy=None):
         if sandbox:
             self.api_url = api_sandbox_url or mangopay.api_sandbox_url
         else:
@@ -42,6 +43,7 @@ class APIRequest(object):
         self.client_id = client_id or mangopay.client_id
         self.passphrase = passphrase or mangopay.passphrase
         self.auth_manager = AuthorizationTokenManager(self, storage_strategy)
+        self.timeout = timeout
 
     def request(self, method, url, data=None, idempotency_key=None, oauth_request=False, **params):
         params = params or {}
@@ -83,7 +85,9 @@ class APIRequest(object):
         try:
             result = requests_session.request(method, url,
                                               data=data,
-                                              headers=headers)
+                                              headers=headers,
+                                              timeout=self.timeout)
+
         except ConnectionError as e:
             msg = '{}'.format(e)
 
@@ -94,6 +98,15 @@ class APIRequest(object):
 
             reraise_as(APIError(msg))
 
+        except Timeout as e:
+            msg = '{}'.format(e)
+
+            if msg:
+                msg = '%s: %s' % (type(e).__name__, msg)
+            else:
+                msg = type(e).__name__
+
+            reraise_as(APIError(msg))
         laps = time.time() - ts
 
         # signal:
