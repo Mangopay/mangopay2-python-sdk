d3dcbdc860c8733ad8998f81f54421037f937453
Margarit Cosmin <cosmin.marga@gmail.com>
2018-09-18 13:21:08 +0300

    MPSDK-270 Headquarters phone number Added headquarter's phone number to Client entity. Upgraded client upload test to check phone number field.

--- mangopay/resources.py
+++ mangopay/resources.py
@@ -4,7 +4,7 @@ from mangopay.signals import pre_save, post_save
 from mangopay.utils import Money, DeclaredUbo
 from . import constants
 from .base import BaseApiModel, BaseApiModelMethods
-
+from .compat import python_2_unicode_compatible
 from .fields import (PrimaryKeyField, EmailField, CharField,
                      BooleanField, DateTimeField, DateField,
                      ManyToManyField, ForeignKeyField,
@@ -12,8 +12,6 @@ from .fields import (PrimaryKeyField, EmailField, CharField,
                      DebitedBankAccountField,
                      ShippingAddressField, RefundReasonField, ListField, ReportTransactionsFiltersField,
                      ReportWalletsFiltersField, BillingField, SecurityInfoField, PlatformCategorizationField)
-
-from .compat import python_2_unicode_compatible
 from .query import InsertQuery, UpdateQuery, SelectQuery, ActionQuery
 
 
@@ -38,6 +36,7 @@ class Client(BaseApiModel):
     platform_categorization = PlatformCategorizationField(api_name='PlatformCategorization')
     platform_url = CharField(api_name='PlatformURL')
     headquarters_address = AddressField(api_name='HeadquartersAddress')
+    headquarters_phone_number = CharField(api_name='HeadquartersPhoneNumber')
     tax_number = CharField(api_name='TaxNumber')
 
     class Meta:
