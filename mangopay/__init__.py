597ecd7a2015e2b712cde37b1555c83db936c065
Luca Monari <monasurf90@gmail.com>
2023-03-01 08:30:07 +0100

    Restored previous logic with improved URLs

--- mangopay/__init__.py
+++ mangopay/__init__.py
@@ -6,12 +6,38 @@ from .utils import env_to_bool, memoize
 client_id = None
 apikey = None
 
+PLENTIFIC_NAMESPACE = os.getenv("PLENTIFIC_NAMESPACE", "uk")
+PLENTIFIC_IS_E2E = env_to_bool("PLENTIFIC_IS_E2E", False)
+
 MANGOPAY_URL = os.getenv("MANGOPAY_URL", "https://api.mangopay.com")
 MANGOPAY_SANDBOX_URL = os.getenv("MANGOPAY_URL", "https://api.sandbox.mangopay.com")
 
-api_url = f'{MANGOPAY_URL}/v2.01/'
-api_sandbox_url = f'{MANGOPAY_SANDBOX_URL}/v2.01/'
+MANGOPAY_DEFAULT_LOCAL_MOCK_SERVER_URL = f"http://localhost:1081/mock-server/{PLENTIFIC_NAMESPACE}/mangopay"
+MANGOPAY_DEFAULT_E2E_MOCK_SERVER_URL = "http://mock-server:1081/mangopay"
+MANGOPAY_IS_MOCK_SERVER_ENABLED = env_to_bool("MANGOPAY_IS_MOCK_SERVER_ENABLED", False)
 
+MANGOPAY_MOCK_SERVER_URL = os.environ.get(
+    "MANGOPAY_MOCK_SERVER_URL",
+    MANGOPAY_DEFAULT_E2E_MOCK_SERVER_URL
+    if PLENTIFIC_IS_E2E
+    else MANGOPAY_DEFAULT_LOCAL_MOCK_SERVER_URL,
+)
+
+
+def _get_api_url():
+    if MANGOPAY_IS_MOCK_SERVER_ENABLED:
+        return MANGOPAY_MOCK_SERVER_URL
+    return MANGOPAY_URL
+
+
+def _get_sandbox_url():
+    if MANGOPAY_IS_MOCK_SERVER_ENABLED:
+        return MANGOPAY_MOCK_SERVER_URL
+    return MANGOPAY_SANDBOX_URL
+
+
+api_url = f'{_get_api_url()}/v2.01/'
+api_sandbox_url = f'{_get_sandbox_url()}/v2.01/'
 temp_dir = None
 api_version = 2.01
 sandbox = True
