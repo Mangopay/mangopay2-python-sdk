d9cdbda68b7353dcddac65695ec3f345d271ed7e
Iulian <40763147+iulian03@users.noreply.github.com>
2025-02-25 10:47:53 +0200

    [feature] identity verification
    * fixed path params config and added Identity Verification hooks
    
    * added support for fetching checks
    
    * fixed kyc documents tests
    
    ---------
    
    Co-authored-by: Iulian Masar <iulian.masar@codegile.com>

--- mangopay/resources.py
+++ mangopay/resources.py
@@ -2317,3 +2317,54 @@ class VirtualAccountAvailability(BaseModel):
         url = {
             SelectQuery.identifier: '/virtual-accounts/availability'
         }
+
+
+class IdentityVerification(BaseModel):
+    creation_date = DateTimeField(api_name='CreationDate')
+    hosted_url = CharField(api_name='HostedUrl')
+    return_url = CharField(api_name='ReturnUrl', required=True)
+    status = CharField(api_name='Status')
+
+    class Meta:
+        verbose_name = 'identity_verification'
+        verbose_name_plural = 'identity_verifications'
+
+        url = {
+            InsertQuery.identifier: '/users/%(user_id)s/identity-verifications',
+            SelectQuery.identifier: '/identity-verifications'
+        }
+
+    def create(self, user_id, idempotency_key=None, **kwargs):
+        path_params = {'user_id': user_id}
+        insert = InsertQuery(self, idempotency_key, path_params, **kwargs)
+        insert.insert_query = self.get_field_dict()
+        return insert.execute()
+
+    def get_checks(self, *args, **kwargs):
+        kwargs['id'] = self.id
+        select = SelectQuery(IdentityVerificationCheck, *args, **kwargs)
+        select.identifier = 'GET_CHECKS'
+        return select.get("", *args, **kwargs)
+
+
+class IdentityVerificationCheck(BaseModel):
+    session_id = CharField(api_name='SessionId')
+    status = CharField(api_name='Status')
+    creation_date = DateTimeField(api_name='CreationDate')
+    last_update = DateTimeField(api_name='LastUpdate')
+    checks = ListField(api_name='Checks')
+
+    class Meta:
+        verbose_name = 'identity_verification_check'
+        verbose_name_plural = 'identity_verifications_checks'
+
+        url = {
+            'GET_CHECKS': '/identity-verifications/%(id)s/checks'
+        }
+
+    @classmethod
+    def get(cls, identity_verification_id, *args, **kwargs):
+        kwargs['id'] = identity_verification_id
+        select = SelectQuery(IdentityVerificationCheck, *args, **kwargs)
+        select.identifier = 'GET_CHECKS'
+        return select.get("", *args, **kwargs)
---
+++ tests/test_identity_verification.py
@@ -0,0 +1,50 @@
+from mangopay.resources import IdentityVerification, IdentityVerificationCheck
+from mangopay.utils import timestamp_from_datetime
+from tests.test_base import BaseTestLive
+
+
+class IdentityVerificationTest(BaseTestLive):
+    _identity_verification = None
+
+    def test_create_identity_verification(self):
+        self.create_new_identity_verification()
+        self.assertIsNotNone(IdentityVerificationTest._identity_verification)
+        self.assertIsNotNone(IdentityVerificationTest._identity_verification.return_url)
+        self.assertIsNotNone(IdentityVerificationTest._identity_verification.hosted_url)
+        self.assertEqual(IdentityVerificationTest._identity_verification.status, 'PENDING')
+
+    def test_get_identity_verification(self):
+        self.create_new_identity_verification()
+        fetched = IdentityVerification.get(IdentityVerificationTest._identity_verification.id)
+
+        self.assertIsNotNone(fetched)
+        self.assertEqual(IdentityVerificationTest._identity_verification.hosted_url, fetched.hosted_url)
+        self.assertEqual(IdentityVerificationTest._identity_verification.return_url, fetched.return_url)
+        self.assertEqual(IdentityVerificationTest._identity_verification.status, fetched.status)
+
+    def test_get_checks(self):
+        self.create_new_identity_verification()
+        # can be fetched in 2 ways:
+
+        # checks: IdentityVerificationCheck = IdentityVerificationCheck.get(
+        #     IdentityVerificationTest._identity_verification.id)
+        checks: IdentityVerificationCheck = IdentityVerificationTest._identity_verification.get_checks()
+
+        self.assertIsNotNone(checks)
+        self.assertEqual(checks.status, 'PENDING')
+        self.assertTrue(timestamp_from_datetime(checks.creation_date) > 0)
+        self.assertTrue(timestamp_from_datetime(checks.last_update) > 0)
+        self.assertIsNotNone(checks.checks)
+
+    @staticmethod
+    def create_new_identity_verification():
+        if IdentityVerificationTest._identity_verification is None:
+            john = BaseTestLive.get_john()
+
+            identity_verification = IdentityVerification()
+            identity_verification.return_url = "https://example.com"
+            identity_verification.tag = "created by the Python SDK"
+
+            IdentityVerificationTest._identity_verification = IdentityVerification(
+                **identity_verification.create(john.id))
+        return IdentityVerificationTest._identity_verification
