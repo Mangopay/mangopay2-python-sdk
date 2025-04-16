e140626fad7e075ae30274fe98135888e222d101
Iulian <40763147+iulian03@users.noreply.github.com>
2025-04-16 16:38:33 +0300

    [feature] added support for pay by bank payin (#396)
    * added support for pay by bank payin
    
    * added event type
    
    ---------
    
    Co-authored-by: Iulian Masar <iulian.masar@codegile.com>

--- mangopay/constants.py
+++ mangopay/constants.py
@@ -108,6 +108,8 @@ EVENT_TYPE_CHOICES = Choices(
     ('PAYIN_NORMAL_CREATED', 'payin_normal_created', 'Payin normal created'),
     ('PAYIN_NORMAL_SUCCEEDED', 'payin_normal_succeeded', 'Payin normal succeeded'),
     ('PAYIN_NORMAL_FAILED', 'payin_normal_failed', 'Payin normal failed'),
+    ('PAYIN_NORMAL_PROCESSING_STATUS_PENDING_SUCCEEDED',
+     'payin_normal_processing_status_pending_succeeded', 'Payin normal processing status pending succeeded'),
     ('PAYOUT_NORMAL_CREATED', 'payout_normal_created', 'Payout normal created'),
     ('PAYOUT_NORMAL_SUCCEEDED', 'payout_normal_succeeded', 'Payout normal succeeded'),
     ('PAYOUT_NORMAL_FAILED', 'payout_normal_failed', 'Payout normal failed'),
