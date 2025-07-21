16c12f9064fa5a10c6e22c1651aaa1ad1f609d9b
Iulian Masar <iulian.masar@codegile.com>
2025-07-21 13:32:07 +0300

    update rate limits to handle 1 and 5 minutes intervals

--- tests/test_rate_limit.py
+++ tests/test_rate_limit.py
@@ -12,3 +12,11 @@ class RateLimit(BaseTest):
         rate_limits = apis.get_rate_limits()
         self.assertIsNotNone(rate_limits)
         self.assertTrue(len(rate_limits) > 0)
+
+        self.assertEqual(1, rate_limits[0].interval_minutes)
+        self.assertEqual(5, rate_limits[1].interval_minutes)
+        self.assertEqual(15, rate_limits[2].interval_minutes)
+        self.assertEqual(30, rate_limits[3].interval_minutes)
+        self.assertEqual(60, rate_limits[4].interval_minutes)
+        self.assertEqual(1440, rate_limits[5].interval_minutes)
+
