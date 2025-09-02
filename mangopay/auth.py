fc1a67071ade1a64265d49dd047ac7cd79ee5620
Iulian Masar <iulian.masar@codegile.com>
2025-09-02 13:27:05 +0300

    updated token expiry threshold

--- mangopay/auth.py
+++ mangopay/auth.py
@@ -89,7 +89,7 @@ class AuthorizationTokenManager(object):
                 token = auth_result[1]
             else:
                 return None
-            token['timestamp'] = time.time() + (int(token['expires_in']) - 60)
+            token['timestamp'] = time.time() + (int(token['expires_in']) - 30)
             self.set_token(token)
 
         return token['token_type'] + ' ' + token['access_token']
